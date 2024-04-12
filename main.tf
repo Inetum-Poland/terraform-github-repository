40855cd0e52a1313dcaafa3debf6b9e47d0a80e8
Aam <amadeusz.kryze@pm.me>
2024-04-12 15:34:12 +0200

    feat(tf_module): add ability to control more aspects of repositories

--- main.tf
+++ main.tf
@@ -9,113 +9,132 @@ terraform {
 
 #trivy:ignore:AVD-GIT-0001 No sensitive information
 resource "github_repository" "repository" {
-  name        = var.name
-  description = var.description
-  topics      = var.topics
-  is_template = var.is_template
-  visibility  = var.visibility
-  auto_init   = var.auto_init
+  name        = var.github_repository.name
+  description = var.github_repository.description
+  topics      = var.github_repository.topics
+  is_template = var.github_repository.is_template
+  visibility  = var.github_repository.visibility
+  auto_init   = var.github_repository.auto_init
 
-  has_discussions = var.has_discussions
-  has_downloads   = var.has_downloads
-  has_issues      = var.has_issues
-  has_projects    = var.has_projects
-  has_wiki        = var.has_wiki
-  homepage_url    = var.homepage_url
-  # private = var.visibility == "private" ? true : false # DEPRICATED
+  has_discussions = var.github_repository.has_discussions
+  has_downloads   = var.github_repository.has_downloads
+  has_issues      = var.github_repository.has_issues
+  has_projects    = var.github_repository.has_projects
+  has_wiki        = var.github_repository.has_wiki
+  homepage_url    = var.github_repository.homepage_url
+  # private = var.github_repository.visibility == "private" ? true : false # DEPRICATED
 
-  allow_merge_commit          = var.allow_merge_commit
-  allow_squash_merge          = var.allow_squash_merge
-  allow_rebase_merge          = var.allow_rebase_merge
-  allow_auto_merge            = var.allow_auto_merge
-  allow_update_branch         = var.allow_update_branch
-  squash_merge_commit_title   = var.squash_merge_commit_title
-  squash_merge_commit_message = var.squash_merge_commit_message
-  merge_commit_title          = var.merge_commit_title
-  merge_commit_message        = var.merge_commit_message
-  delete_branch_on_merge      = var.delete_branch_on_merge
+  allow_merge_commit          = var.github_repository.allow_merge_commit
+  allow_squash_merge          = var.github_repository.allow_squash_merge
+  allow_rebase_merge          = var.github_repository.allow_rebase_merge
+  allow_auto_merge            = var.github_repository.allow_auto_merge
+  allow_update_branch         = var.github_repository.allow_update_branch
+  squash_merge_commit_title   = var.github_repository.squash_merge_commit_title
+  squash_merge_commit_message = var.github_repository.squash_merge_commit_message
+  merge_commit_title          = var.github_repository.merge_commit_title
+  merge_commit_message        = var.github_repository.merge_commit_message
+  delete_branch_on_merge      = var.github_repository.delete_branch_on_merge
 
-  license_template                        = var.license_template == null ? (var.visibility == "private" ? "unlicense" : "mit") : var.license_template
-  archive_on_destroy                      = var.archive_on_destroy
-  web_commit_signoff_required             = var.web_commit_signoff_required
-  vulnerability_alerts                    = var.vulnerability_alerts
-  ignore_vulnerability_alerts_during_read = var.ignore_vulnerability_alerts_during_read # true
+  license_template                        = var.github_repository.license_template == null ? (var.github_repository.visibility == "private" ? "unlicense" : "mit") : var.github_repository.license_template
+  archive_on_destroy                      = var.github_repository.archive_on_destroy
+  web_commit_signoff_required             = var.github_repository.web_commit_signoff_required
+  vulnerability_alerts                    = var.github_repository.vulnerability_alerts
+  ignore_vulnerability_alerts_during_read = var.github_repository.ignore_vulnerability_alerts_during_read # true
 
   dynamic "template" {
-    for_each = var.template != null ? [1] : []
+    for_each = var.github_repository.template != null ? [1] : []
 
     content {
-      owner                = var.template.owner
-      repository           = var.template.repository
-      include_all_branches = var.template.include_all_branches
+      owner                = var.github_repository.template.owner
+      repository           = var.github_repository.template.repository
+      include_all_branches = var.github_repository.template.include_all_branches
     }
   }
 
   dynamic "pages" {
-    for_each = var.pages != null ? [1] : []
+    for_each = var.github_repository.pages != null ? [1] : []
 
     content {
-      build_type = var.pages.build_type
-      cname      = var.pages.cname
+      build_type = var.github_repository.pages.build_type
+      cname      = var.github_repository.pages.cname
 
 
       dynamic "source" {
-        for_each = var.pages.source != null ? [1] : []
+        for_each = var.github_repository.pages.source != null ? [1] : []
 
         content {
-          branch = var.pages.source.branch
-          path   = var.pages.source.path
+          branch = var.github_repository.pages.source.branch
+          path   = var.github_repository.pages.source.path
         }
       }
     }
   }
 
   dynamic "security_and_analysis" {
-    for_each = var.visibility == "public" ? [1] : []
+    for_each = var.github_repository.visibility == "public" ? [1] : []
 
     content {
       # dynamic "advanced_security" {
-      #   for_each = var.visibility == "public" ? [1] : []
+      #   for_each = var.github_repository.visibility == "public" ? [1] : []
 
       #   content {
-      #     status = var.security_and_analysis.advanced_security.status
+      #     status = var.github_repository.security_and_analysis.advanced_security.status
       #   }
       # }
 
       dynamic "secret_scanning" {
-        for_each = var.visibility == "public" ? [1] : []
+        for_each = var.github_repository.visibility == "public" ? [1] : []
 
         content {
-          status = var.security_and_analysis.secret_scanning.status
+          status = var.github_repository.security_and_analysis.secret_scanning.status
         }
       }
 
       dynamic "secret_scanning_push_protection" {
-        for_each = var.visibility == "public" ? [1] : []
+        for_each = var.github_repository.visibility == "public" ? [1] : []
 
         content {
-          status = var.security_and_analysis.secret_scanning_push_protection.status
+          status = var.github_repository.security_and_analysis.secret_scanning_push_protection.status
         }
       }
     }
   }
 }
 
-# output "security_and_analysis" {
-#   value = null
+#trivy:ignore:AVD-GIT-0004 No need to sign commits
+resource "github_branch_protection" "branch" {
+  for_each = var.github_branch_protection
 
-#   precondition {
-#     condition = (
-#       (var.visibility == "private" && (
-#         var.security_and_analysis.secret_scanning_push_protection.status == "disabled" ||
-#         var.security_and_analysis.secret_scanning.status == "disabled" ||
-#         var.security_and_analysis.advanced_security.status == "disabled"
-#       )) || (var.visibility == "public" && (
-#         var.security_and_analysis.secret_scanning_push_protection.status == "enabled" &&
-#         var.security_and_analysis.secret_scanning.status == "enabled" &&
-#         var.security_and_analysis.advanced_security.status == "enabled"
-#       ))
-#     )
-#     error_message = "Must be enabled for public repository."
-#   }
-# }
+  repository_id = github_repository.repository.id
+  pattern       = each.value.pattern
+
+  allows_deletions                = each.value.allows_deletions
+  allows_force_pushes             = each.value.allows_force_pushes
+  enforce_admins                  = each.value.enforce_admins
+  force_push_bypassers            = each.value.force_push_bypassers
+  lock_branch                     = each.value.lock_branch
+  require_conversation_resolution = each.value.require_conversation_resolution
+  require_signed_commits          = each.value.require_signed_commits
+  required_linear_history         = each.value.required_linear_history
+
+  dynamic "required_pull_request_reviews" {
+    for_each = each.value.required_pull_request_reviews != null ? [1] : []
+
+    content {
+      dismiss_stale_reviews           = each.value.required_pull_request_reviews.dismiss_stale_reviews
+      require_code_owner_reviews      = each.value.required_pull_request_reviews.require_code_owner_reviews
+      require_last_push_approval      = each.value.required_pull_request_reviews.require_last_push_approval
+      required_approving_review_count = each.value.required_pull_request_reviews.required_approving_review_count
+      restrict_dismissals             = each.value.required_pull_request_reviews.restrict_dismissals
+    }
+  }
+
+  dynamic "required_status_checks" {
+    for_each = each.value.required_status_checks != null ? [1] : []
+
+    content {
+      strict   = each.value.required_status_checks.strict
+      contexts = each.value.required_status_checks.contexts
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,244 +1,126 @@
-variable "name" {
-  type        = string
-  description = "The name of the repository."
+variable "github_repository" {
+  type = object({
+    name        = string
+    description = optional(string)
+    topics      = optional(list(string), [])
+    is_template = optional(bool, false)
+    visibility  = optional(string, "private")
+    auto_init   = optional(bool, true)
+
+    has_downloads   = optional(bool, false)
+    has_issues      = optional(bool, false)
+    has_projects    = optional(bool, false)
+    has_wiki        = optional(bool, false)
+    has_discussions = optional(bool, false)
+    homepage_url    = optional(string)
+
+    allow_merge_commit          = optional(bool, true)
+    allow_squash_merge          = optional(bool, true)
+    allow_rebase_merge          = optional(bool, true)
+    allow_auto_merge            = optional(bool, false)
+    allow_update_branch         = optional(bool, true)
+    squash_merge_commit_title   = optional(string, "PR_TITLE")
+    squash_merge_commit_message = optional(string, "PR_BODY")
+    merge_commit_title          = optional(string, "PR_TITLE")
+    merge_commit_message        = optional(string, "PR_BODY")
+    delete_branch_on_merge      = optional(bool, true)
+
+    license_template                        = optional(string)
+    archive_on_destroy                      = optional(bool, true)
+    web_commit_signoff_required             = optional(bool, false)
+    vulnerability_alerts                    = optional(bool, true)
+    ignore_vulnerability_alerts_during_read = optional(bool, true)
+
+    template = optional(object({
+      owner                = string
+      repository           = any
+      include_all_branches = bool
+    }), null)
+
+    pages = optional(object({
+      source = object({
+        branch = string
+        path   = string
+      })
+      build_type = string
+      cname      = string
+    }), null)
+
+    security_and_analysis = optional(object({
+      advanced_security = optional(object({
+        status = optional(string, "enabled")
+      }), {})
+      secret_scanning = optional(object({
+        status = optional(string, "enabled")
+      }), {})
+      secret_scanning_push_protection = optional(object({
+        status = optional(string, "enabled")
+      }), {})
+    }), {})
+  })
+
+  description = "Repository settings object `github_repository`."
 
   validation {
-    condition     = length(var.name) > 0 && !contains(["_"], var.name)
-    error_message = "Must not be empty"
+    condition     = length(var.github_repository.name) > 0 && !contains(["_"], var.github_repository.name)
+    error_message = "Must not be empty nor contain an underscore (_)."
   }
-}
-
-variable "description" {
-  type        = string
-  description = "A description of the repository"
-  default     = null
-}
-
-variable "topics" {
-  type        = list(string)
-  description = "The list of topics of the repository."
-  default     = []
-}
-
-variable "is_template" {
-  type        = bool
-  description = "Whether the repository is a template."
-  default     = false
-}
-
-variable "visibility" {
-  type        = string
-  description = "The visibility of the repository."
-  default     = "private"
 
   validation {
-    condition     = contains(["private", "public"], var.visibility)
+    condition     = contains(["private", "public"], var.github_repository.visibility)
     error_message = "Must be one of 'private', 'public'."
   }
-}
-
-variable "auto_init" {
-  type        = bool
-  description = "Automatically create the default branch."
-  default     = true
-}
-
-variable "has_downloads" {
-  type        = bool
-  description = "Whether the repository has downloads."
-  default     = false
-}
-
-variable "has_issues" {
-  type        = bool
-  description = "Whether the repository has issues."
-  default     = false
-}
-
-variable "has_discussions" {
-  type        = bool
-  description = "Whether the repository has discussions."
-  default     = false
-}
-
-variable "has_projects" {
-  type        = bool
-  description = "Whether the repository has projects."
-  default     = false
-}
-
-variable "has_wiki" {
-  type        = bool
-  description = "Whether the repository has wiki."
-  default     = false
-}
-
-variable "homepage_url" {
-  type        = string
-  description = "The URL of the repository's homepage."
-  default     = null
-}
-
-variable "allow_merge_commit" {
-  type        = bool
-  description = "Whether to allow merge commits."
-  default     = true
-}
-
-variable "allow_squash_merge" {
-  type        = bool
-  description = "Whether to allow squash merges."
-  default     = true
-}
-
-variable "allow_rebase_merge" {
-  type        = bool
-  description = "Whether to allow rebase merges."
-  default     = true
-}
-
-variable "allow_auto_merge" {
-  type        = bool
-  description = "Whether to allow auto-merges."
-  default     = false
-}
-
-variable "allow_update_branch" {
-  type        = bool
-  description = "Whether to allow update branches."
-  default     = true
-}
-
-variable "squash_merge_commit_title" {
-  type        = string
-  description = "The title of the merge commit."
-  default     = "PR_TITLE"
-}
-
-variable "squash_merge_commit_message" {
-  type        = string
-  description = "The message of the merge commit."
-  default     = "PR_BODY"
-}
-
-variable "merge_commit_title" {
-  type        = string
-  description = "The title of the merge commit."
-  default     = "PR_TITLE"
-}
-
-variable "merge_commit_message" {
-  type        = string
-  description = "The message of the merge commit."
-  default     = "PR_BODY"
-}
-
-variable "delete_branch_on_merge" {
-  type        = bool
-  description = "Whether to delete the branch on merge."
-  default     = true
-}
-
-variable "license_template" {
-  type        = string
-  description = "The license template of the repository."
-  default     = null
-}
-
-variable "archive_on_destroy" {
-  type        = bool
-  description = "Whether to archive the repository on destroy."
-  default     = true
-}
-
-variable "web_commit_signoff_required" {
-  type        = bool
-  description = "Whether to require signed commits."
-  default     = false
-}
-
-variable "ignore_vulnerability_alerts_during_read" {
-  type        = bool
-  description = "Whether to ignore vulnerability alerts during read."
-  default     = true
-}
-
-variable "vulnerability_alerts" {
-  type        = string
-  description = "Whether to enable vulnerability alerts."
-  default     = true
-}
-
-variable "template" {
-  type = object({
-    owner                = string
-    repository           = any
-    include_all_branches = bool
-  })
-
-  description = "Template repository settings object `github_repository`."
-  default     = null
-}
-
-variable "pages" {
-  type = object({
-    source = object({
-      branch = string
-      path   = string
-    })
-
-    build_type = string
-    cname      = string
-  })
-
-  description = "Pages repository settings object `github_repository`."
-  default     = null
-}
-
-variable "security_and_analysis" {
-  type = object({
-    advanced_security = object({
-      status = string
-    })
-    secret_scanning = object({
-      status = string
-    })
-    secret_scanning_push_protection = object({
-      status = string
-    })
-  })
-
-  description = "Security and analysis repository settings object `github_repository`."
-  default = {
-    advanced_security = {
-      status = "enabled"
-    }
-    secret_scanning = {
-      status = "enabled"
-    }
-    secret_scanning_push_protection = {
-      status = "enabled"
-    }
-  }
 
   validation {
     condition = (
-      contains(["enabled", "disabled"], coalesce(var.security_and_analysis, { advanced_security = { status = "enabled" } }).advanced_security.status, )
+      contains(["enabled", "disabled"], coalesce(var.github_repository.security_and_analysis, { advanced_security = { status = "enabled" } }).advanced_security.status, )
     )
     error_message = "Must be enabled or disabled"
   }
 
   validation {
     condition = (
-      contains(["enabled", "disabled"], coalesce(var.security_and_analysis, { secret_scanning = { status = "enabled" } }).secret_scanning.status)
+      contains(["enabled", "disabled"], coalesce(var.github_repository.security_and_analysis, { secret_scanning = { status = "enabled" } }).secret_scanning.status)
     )
     error_message = "Must be enabled or disabled"
   }
 
   validation {
     condition = (
-      contains(["enabled", "disabled"], coalesce(var.security_and_analysis, { secret_scanning_push_protection = { status = "enabled" } }).secret_scanning_push_protection.status)
+      contains(["enabled", "disabled"], coalesce(var.github_repository.security_and_analysis, { secret_scanning_push_protection = { status = "enabled" } }).secret_scanning_push_protection.status)
     )
     error_message = "Must be enabled or disabled"
   }
+}
+
+variable "github_branch_protection" {
+  type = map(object({
+    pattern = string
+
+    allows_deletions                = optional(bool, false)
+    allows_force_pushes             = optional(bool, false)
+    enforce_admins                  = optional(bool, false)
+    force_push_bypassers            = optional(list(string), ["Inetum-Poland/principal"])
+    lock_branch                     = optional(bool, false)
+    require_conversation_resolution = optional(bool, true)
+    require_signed_commits          = optional(bool, false)
+    required_linear_history         = optional(bool, false)
+
+    required_pull_request_reviews = optional(object({
+      dismiss_stale_reviews           = optional(bool, true)
+      require_code_owner_reviews      = optional(bool, true)
+      require_last_push_approval      = optional(bool, true)
+      required_approving_review_count = optional(number, 1)
+      restrict_dismissals             = optional(bool, false)
+    }), {})
+
+    required_status_checks = optional(object({
+      contexts = optional(list(string), ["pre_commit"])
+      strict   = optional(bool, true)
+    }), {})
+  }))
+
+  description = "Branch protection repository settings object `github_repository`."
+
+  default = { main = { pattern = "main" } }
 }
