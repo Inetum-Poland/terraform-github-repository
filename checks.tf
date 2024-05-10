77915a93db745de5ca16668fb200725f0649e0f4
maciej łakomski <lakomskimaciej.kontakt@gmail.com>
2024-05-10 15:30:39 +0200

    feat(tf): fix the check repo name underscore

--- checks.tf
+++ checks.tf
@@ -9,8 +9,8 @@ check "repo_name_check" {
 
 check "repo_name_underscore_check" {
   assert {
-    condition     = can(regex("^[0-9A-Za-z-]+$", var.github_repository.name))
-    error_message = "Repository name must not contain an underscore (_)."
+    condition     = can(regex("^\\.?[0-9A-Za-z-]+$", var.github_repository.name))
+    error_message = "Name does not meet naming convention standard"
   }
 }
 
